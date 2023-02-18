e8846e5ac2321c9ddb04476a8a7e79919391537a
ZheChen1999 <1015054300@qq.com>
2023-02-18 22:59:09 +0000

    v2

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,13 +2,68 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import QRCode from 'qrcode.react';
+import moment from "moment"
+
 
 export default function Home() {
-  
-  const [user, setUser] = useState({});
 
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    let difference = +new Date(`05/15/${year}`) - +new Date();
   
+    let timeLeft = {};
   
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      };
+    }
+    return timeLeft;
+  }
+
+
+  const [user, setUser] = useState({});
+  const [timer, setTimer] = useState('00:00:00')
+  const [refresh, setRefresh] = useState({})
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setRefresh((r) => r+1);
+      setTimer(moment(new Date().getTime()).format("HH:mm:ss"))  
+    },1000)
+
+    return (()=>{
+      clearTimeout(timeoutId)
+    })
+
+  }, [refresh])
 
   useEffect(() => {
     // get token from local storage
@@ -28,7 +83,6 @@ export default function Home() {
       })
       .catch((error) => {
         console.log(error);
-        console.log(token)
       });
   }, []);
 
@@ -66,9 +120,10 @@ export default function Home() {
     }, 1500);
   };
 
+
   return (
     <div >
-      <div>
+      <div style={{display:"flex",justifyContent:"center"}}>
         <img
             alt="profile"
             //src={user.profile}
@@ -76,31 +131,73 @@ export default function Home() {
           />
       </div>
       
-      <div style={{margin:"15%"}}>
+      <div style={{margin:"11%"}}>
         <QRCode
           id="qrCode"
-          level="H"
+          level="Q"
           value={user}
-          size={230} // 二维码的大小
+          size={210} // 二维码的大小
           fgColor="#000000" // 二维码的颜色
           style={{ margin: 'auto' }}
           
         /> 
       </div>
-      <div>
+      <div style={{display:"flex",justifyContent:"center"}}>
         <img
             alt="profile"
             //src={user.profile}
             src={require('./asset/middle.gif')}
           />
       </div>
-      <div>
-        <img
-            alt="profile"
-            //src={user.profile}
-            src={require('./asset/bottom.jpg')}
-          />
+
+      <div style={{textAlign:"center", margin:"3% 0"}}>
+        <span style={{fontSize:"160%",fontWeight:"670",fontFamily:"",opacity:"80%"}}>
+          {timer}
+        </span>
       </div>
+
+      <div style={{borderTop:"0.2px solid rgba(192,192,192,70%)", borderBottom:"0.2px solid rgba(192,192,192,70%)", margin:"5% 3% 0", padding:"3% 0"}}>
+        {
+          timerComponents.length ? 
+          <div style={{display:"flex",justifyContent:"center"}}>
+            <div style={{textAlign:"center"}}>
+              <h1 style={{lineHeight:"100%",fontSize:"160%",fontWeight:"670",fontFamily:"",opacity:"80%"}}>{timeLeft.days}</h1>
+              <h1 style={{fontWeight:"300", fontSize:"80%"}}>days</h1>
+            </div>
+
+            <div style={{lineHeight:"100%",margin:"0 3%", fontSize:"180%",fontWeight:"670",fontFamily:"",opacity:"80%"}}>:</div>
+
+            <div style={{textAlign:"center"}}>
+              <h1 style={{lineHeight:"100%",fontSize:"160%",fontWeight:"670",fontFamily:"",opacity:"80%"}}>{timeLeft.hours>=10?timeLeft.hours:<span>0{timeLeft.hours}</span>}</h1>
+              <h1 style={{fontWeight:"300", fontSize:"80%"}}>hrs</h1>
+            </div>
+            
+            <div style={{lineHeight:"100%",margin:"0 3%",fontSize:"180%",fontWeight:"670",fontFamily:"",opacity:"80%"}}>:</div>
+
+            <div style={{textAlign:"center"}}>
+              <h1 style={{lineHeight:"100%",fontSize:"160%",fontWeight:"670",fontFamily:"",opacity:"80%"}}>{timeLeft.minutes>=10?timeLeft.minutes:<span>0{timeLeft.minutes}</span>}</h1>
+              <h1 style={{fontWeight:"300", fontSize:"80%"}}>mins</h1>
+            </div>
+
+            <div style={{lineHeight:"100%",margin:"0 3%",fontSize:"180%",fontWeight:"670",fontFamily:"",opacity:"80%"}}>:</div>
+
+            <div style={{textAlign:"center"}}>
+              <h1 style={{lineHeight:"100%",fontSize:"160%",fontWeight:"670",fontFamily:"",opacity:"80%"}}>{timeLeft.seconds>=10?timeLeft.seconds:<span>0{timeLeft.seconds}</span>}</h1>
+              <h1 style={{fontWeight:"300", fontSize:"80%"}}>seconds</h1>
+            </div>
+
+            
+          </div> : 
+          <span>Time's up!</span>
+          }
+      </div>
+
+      <div style={{padding:"3%"}}>
+        <h1 style={{fontSize:"70%", fontWeight:"700", opacity:"80%"}}>90 day</h1>
+        <h1 style={{fontSize:"70%", opacity:"80%"}}>Travel in the Southampton Zone for up to 90 days from activation</h1>
+        <h1 style={{fontSize:"70%", opacity:"80%"}}>Expires 00:00, Fri 29th December</h1>
+      </div>
+
 
       <div className="flex p-2">
           <div className="w-full text-center">
---
+++ read_code.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 18 14:29:18 2023
+
+@author: zt7n22
+"""
+
+import time
+import win32gui, win32ui, win32con, win32api
+#!-*- coding:utf-8 -*-
+import os
+import requests
+from io import BytesIO
+from pyzbar import pyzbar
+from PIL import Image,ImageEnhance
+import qrcode,os
+import time
+
+def window_capture(filename):
+    hwnd = 0  # 窗口的编号，0号表示当前活跃窗口
+    # 根据窗口句柄获取窗口的设备上下文DC（Divice Context）
+    hwndDC = win32gui.GetWindowDC(hwnd)
+    # 根据窗口的DC获取mfcDC
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+    # mfcDC创建可兼容的DC
+    saveDC = mfcDC.CreateCompatibleDC()
+    # 创建bigmap准备保存图片
+    saveBitMap = win32ui.CreateBitmap()
+    # 获取监控器信息
+    MoniterDev = win32api.EnumDisplayMonitors(None, None)
+    #print(MoniterDev)
+    w = MoniterDev[0][2][2]
+    h = MoniterDev[0][2][3]
+    print(w)
+    print(h)
+    # 为bitmap开辟空间
+    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+    # 高度saveDC，将截图保存到saveBitmap中
+    saveDC.SelectObject(saveBitMap)
+    # 截取从左上角（0，0）长宽为（w，h）的图片
+    saveDC.BitBlt((0, 0), (int(w/2), int(h/2)), mfcDC, (0, 0), win32con.SRCCOPY)
+    saveBitMap.SaveBitmapFile(saveDC, filename)
+ 
+def get_ewm(img_adds):
+    """ 读取二维码的内容： img_adds：二维码地址（可以是网址也可是本地地址 """
+    if os.path.isfile(img_adds):
+        # 从本地加载二维码图片
+        img = Image.open(img_adds)
+    else:
+        # 从网络下载并加载二维码图片
+        rq_img = requests.get(img_adds).content
+        img = Image.open(BytesIO(rq_img))
+ 
+    # img.show()  # 显示图片，测试用
+ 
+    txt_list = pyzbar.decode(img)
+   
+    data = ""
+    for txt in txt_list:
+        barcodeData = txt.data.decode("utf-8")
+        data = data + barcodeData
+    return data
+
+def cap_jiexi_bus_code(is_jiexi = True):
+    if is_jiexi:
+        i=0
+        while True:
+            window_capture("haha" + str(i) + ".jpg")
+            jiexi_text = get_ewm("haha" + str(i) + ".jpg")
+            time.sleep(1)
+            print(jiexi_text)
+            with open('data.txt','w') as f:    #设置文件对象
+                f.write(jiexi_text)                 #将字符串写入文件中
+            print("jiexi successfully!")
+try:
+    cap_jiexi_bus_code(True)
+
+except KeyboardInterrupt:
+    print('jiexi wrong!')
+
+except Exception as e:
+    print(e)
